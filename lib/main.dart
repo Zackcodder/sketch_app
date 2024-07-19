ddd2b1afbe55a999f428e5a3a66a02389aa0e58f
zack <paul.udah@flickssolutions.ng>
2024-07-19 22:36:08 +0100

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => BackgroundProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
